1fef6af77158aff7142fc18365a6e336c83518a9
m-masih <m.masih.rafiee@gmail.com>
2025-07-13 19:20:22 +0330

    created variables and created the cookie

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-const cookie = {
-  label: "cookie",
-  price: 42,
-};
+// interducing variables to access HTML elements
+//spendable money
+const walletpath = document.querySelector(".wallet");
+//the main cookie button
+const cookiepath = document.querySelector(".mainCookie");
+//the upgrades tab
+const addonspath = document.querySelector(".addons");
+let clickMultiplier = 1;
+let wallet = 0;
+let totalincome = 0;
+
+function income (incomeValue){
+    wallet = wallet + incomeValue;
+    totalincome = totalincome + incomeValue;
+    walletpath.textContent = wallet;
+}
+
+function cookieClicked(){
+    income(1*clickMultiplier);
+    console.log(totalincome);
+}
+
+cookiepath.addEventListener("click", cookieClicked);
+
