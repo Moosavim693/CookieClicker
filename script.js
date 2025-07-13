eb9f6dbb4293af9fdfd0bee13b5bd90acf1bf8d0
m-masih <m.masih.rafiee@gmail.com>
2025-07-13 20:23:15 +0330

    fixed value not showing and added upgrade function

--- script.js
+++ script.js
@@ -5,20 +5,51 @@ const walletpath = document.querySelector(".wallet");
 const cookiepath = document.querySelector(".mainCookie");
 //the upgrades tab
 const addonspath = document.querySelector(".addons");
+//total income 
+const totalincomepath = document.querySelector("#totalIncome");
+// cookies per second
+const CPSpath = document.querySelector("#cps");
+//upgrade button
+const upgradeButtonpath = {
+     itself : document.querySelector("#upgradebtn"),
+     cost : document.querySelector(".upgradeCost"),
+     tier : document.querySelector(".upgradeTier"),
+}
+let upgradeButton = { cost : 100, modifier : 2,};
+
+let CPS = 0.0
 let clickMultiplier = 1;
 let wallet = 0;
 let totalincome = 0;
 
-function income (incomeValue){
+function income(incomeValue) {
+    
     wallet = wallet + incomeValue;
     totalincome = totalincome + incomeValue;
     walletpath.textContent = wallet;
+    totalincomepath.textContent = totalincome;
+
 }
 
-function cookieClicked(){
-    income(1*clickMultiplier);
+function cookieClicked() {
+    income(1 * clickMultiplier);
     console.log(totalincome);
 }
 
+function upgrade(){
+    if (wallet > upgradeButton.cost) {
+        wallet = wallet - upgradeButton.cost;
+        walletpath.textContent = wallet;
+        clickMultiplier = clickMultiplier + upgradeButton.modifier;
+        upgradeButton.modifier = upgradeButton.modifier + (upgradeButton.modifier/2);
+        upgradeButton.cost = upgradeButton.cost *8;   
+        upgradeButtonpath.cost.textContent = upgradeButton.cost;
+        upgradeButtonpath.tier.textContent ++;
+    }
+}
+
 cookiepath.addEventListener("click", cookieClicked);
+upgradeButtonpath.itself.addEventListener("click", upgrade);
+
+
 
